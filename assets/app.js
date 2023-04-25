f852979a08baa0e8ecbe89363115241f73a0b243
lsoulier42 <louisesoulier789@gmail.com>
2023-04-25 15:54:38 +0200

    fix front

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,14 @@
 var $ = require('jquery');
 global.$ = global.jQuery = $;
 window.$ = window.jQuery = $
-import('bootstrap');
-
+require('bootstrap/dist/js/bootstrap.bundle');
 
 import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
+
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+})
